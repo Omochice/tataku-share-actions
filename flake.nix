0e69cee45db0ba8cea81d3c67186a311bd66a578
Omochice <mochice.mls.ntl@gmail.com>
2025-06-12 01:00:08 +0900

    chore: initialize

---
+++ flake.nix
@@ -0,0 +1,93 @@
+{
+  description = "Shareable actions for tataku.vim";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nur = {
+      url = "github:Omochice/nur-packages";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      treefmt-nix,
+      flake-utils,
+      nur,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nur.overlays.default ];
+        };
+        treefmt = treefmt-nix.lib.evalModule pkgs (
+          { ... }:
+          {
+            settings.global.excludes = [
+              "CHANGELOG.md"
+            ];
+            programs = {
+              # keep-sorted start block=yes
+              formatjson5 = {
+                enable = true;
+                indent = 2;
+              };
+              jsonfmt.enable = true;
+              keep-sorted.enable = true;
+              mdformat.enable = true;
+              nixfmt.enable = true;
+              yamlfmt = {
+                enable = true;
+                settings = {
+                  formatter = {
+                    type = "basic";
+                    retain_line_breaks_single = true;
+                  };
+                };
+              };
+            };
+            # keep-sorted end
+          }
+        );
+        runAs =
+          name: runtimeInputs: text:
+          let
+            program = pkgs.writeShellApplication {
+              inherit name runtimeInputs text;
+            };
+          in
+          {
+            type = "app";
+            program = "${program}/bin/${name}";
+          };
+      in
+      {
+        formatter = treefmt.config.build.wrapper;
+        checks = {
+          formatting = treefmt.config.build.check self;
+        };
+        apps = {
+          check-actions =
+            ''
+              actionlint
+              ghalint run
+            ''
+            |> runAs "check-actions" [
+              pkgs.actionlint
+              pkgs.ghalint
+            ];
+          check-renovate-config =
+            "renovate-config-validator renovate.json5" |> runAs "check-renovate-config" [ pkgs.renovate ];
+        };
+      }
+    );
+}
